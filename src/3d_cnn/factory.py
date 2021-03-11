864cd5b8d6f57ed6ee1a3ab14608589922470dff
EC2 Default User <ec2-user@ip-172-16-165-212.ec2.internal>
2021-03-11 13:15:24 +0000

    modified some of the transforms and created a new training model

--- src/3d_cnn/factory.py
+++ src/3d_cnn/factory.py
@@ -53,7 +53,7 @@ def _get_xforms(mode="train", keys=("image", "label"), img_size=(192, 192, 16)):
                 RandCropByPosNegLabeld(keys, label_key=keys[1],
                                        spatial_size=img_size,
                                        num_samples=3),
-                RandGaussianNoised(keys[0], prob=0.15, std=0.01),
+                RandGaussianNoised(keys[0], prob=0.20, std=0.01),
                 RandFlipd(keys, spatial_axis=0, prob=0.5),
                 RandFlipd(keys, spatial_axis=1, prob=0.5),
                 RandFlipd(keys, spatial_axis=2, prob=0.5),
