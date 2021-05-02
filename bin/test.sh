66763cd6422edebe9a64f2a88e70070f0f7e79d0
Raian Portela <raianportela@hotmail.com>
2021-05-02 14:52:28 -0300

    feat: added an ensemble method  for testing using MONAI.

--- bin/test.sh
+++ bin/test.sh
@@ -11,4 +11,11 @@ test() {
     python3 ./src/cnn/main.py test ${conf} --snapshot ${snapshot} --output ${test} --gpu ${gpu}
 }
 
-test model002 model_key_metric=0.9104.pt
+# test model002 model_key_metric=0.9104.pt
+#test model003 SegResNet_focalLoss_fold0_0.9094.pt
+#test model003 SegResNet_focalLoss_fold1_0.9342.pt
+#test model003 SegResNet_focalLoss_fold2_0.9309.pt
+#test model003 SegResNet_focalLoss_fold3_0.9181.pt
+#test model003 SegResNet_focalLoss_fold4_0.9368.pt
+
+test model003
